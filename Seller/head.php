8a26a15255086a9d04a9e4da1051c42550157d19
UR <usmangujjarnew@gmail.com>
2025-09-13 20:04:37 +0500

    Initial commit

---
+++ Seller/head.php
@@ -0,0 +1,10 @@
+ <head>
+    <meta charset="utf-8">
+    <title>Seller Side</title>
+    
+    <!-- site css -->
+    <link rel="stylesheet" href="dist/css/site.min.css">
+     
+    <script type="text/javascript" src="dist/js/site.min.js"></script>
+  
+</head>
